04f410486a823e39aaa90ce574b5a77555f41133
keks <keksundso@hotmail.de>
2017-10-07 14:19:38 +0200

    tkinter testes

--- test.py
+++ test.py
@@ -1,5 +1,51 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
+import Tkinter as Tk
+import tkFileDialog
 
-print sorted([1,5,3,7,2,8,3,2])
+import sys
+
+
+root = Tk.Tk()
+root.wm_title("mouse2human")
+
+T = Tk.Text(root, height=2, width=30)
+T.pack()
+T.insert(Tk.END, "Just a text Widget\nin two lines\n")
+
+def browse_file():
+    global fname
+    fname = tkFileDialog.askopenfilename()
+    print "Selected File is:"
+    print fname
+    broButton = Tk.Button(master = root, text = 'Run', width = 6, command=run_button)
+    broButton.pack(side=Tk.LEFT, padx = 2, pady=2)    
+
+
+c = Tk.Entry(root)
+c.pack()
+
+
+k = Tk.Entry(root)
+k.pack()
+
+def run_button():
+    global  var_c 
+    var_c = c.get()
+    global  var_k
+    var_k =   k.get()
+    root.quit()
+
+
+
+
+
+
+
+broButton = Tk.Button(master = root, text = 'Browse', width = 6, command=browse_file)
+broButton.pack(side=Tk.LEFT, padx = 2, pady=2)  
+
+
+
+Tk.mainloop()
+print fname
+print var_k
+print var_c
